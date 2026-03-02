8ff458be788d3484aba750619e3e1c8e7a178796
Rodion <rodionvitenberg@gmail.com>
2026-03-02 18:03:57 +0600

    2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -152,6 +152,7 @@ CELERY_TIMEZONE = 'UTC'
 # CORS CONFIGURATION
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://localhost:8000",
     "http://127.0.0.1:3000",
     "http://92.113.146.158:3000",
     "http://92.113.146.158",
